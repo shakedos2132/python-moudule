2eb60bf0cc201180723b1c49cb70197d3ed7f53f
Shaked Alkobi <shakedos2132@gmail.com>
2026-03-05 18:36:44 +0200

    added functiom

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 print ("Hello, World")
+
+# for i in range(5):
+#     print (i)
+
+# print( 2 + 3)
+
+# USER_NAME = input("What is your name? : ")
+# print ("Hello, " + USER_NAME)
+
+# def greet(name):
+#     print ("Hello, " + name)
+
+# greet("Alice")
+# greet("Bob")   
+# greet("Charlie")
+
+def add(num1, num2):
+    print (num1 + num2)
+    print (num1 - num2)
+
+add(5, 10)
+add(3, 7)
+sub(5,8)
